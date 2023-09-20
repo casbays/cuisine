8dbf72c80e767e24b2a7a6adf50329aeeb5e1ef9
casbays <casbays@outlook.com>
2023-09-20 14:09:03 +0800

    test: auto deploy

--- docs/.vuepress/configs/navbar/en.ts
+++ docs/.vuepress/configs/navbar/en.ts
@@ -8,29 +8,11 @@ export const navbarEN: NavbarConfig = [
     {
         text: '使用',
         children: [{
-            text: '登录',
-            link: '/zh/usages/login.md'
+            text: '小炒',
+            link: '/usages/login.md'
         }, {
-            text: '首页',
-            link: '/zh/usages/dashboard.md'
-        }, {
-            text: '任务中心',
-            link: '/zh/usages/task.md'
-        }, {
-            text: '基金',
-            link: '/zh/usages/fund.md'
-        }, {
-            text: '投资者关系',
-            link: '/zh/usages/ir.md'
-        }, {
-            text: '投前协作',
-            link: '/zh/usages/preivs.md'
-        }, {
-            text: '投后管理',
-            link: '/zh/usages/postivs.md'
-        }, {
-            text: '报表中心',
-            link: '/zh/usages/report.md'
+            text: '干锅',
+            link: '/usages/dashboard.md'
         }]
     },
     {
